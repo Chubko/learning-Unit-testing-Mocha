ead96254438df9096fda5b15a4a320b80fb3b134
Liydmyla Chubko <luda.chubko@gmail.com>
2021-07-11 14:38:25 +0300

    Retries, skip, only, async tests

---
+++ .mocharc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // ui: 'bdd',
+    timeout: 2000,
+    slow: 150,
+    reporter: 'spec',
+    retries: 0,
+    ui: 'bdd', 'watch-files': ['test/**/*.js']
+}
---
+++ test/async.app.spec.js
@@ -0,0 +1,47 @@
+const expect = require('chai').expect;
+const axios = require('axios');
+
+describe('async test case', function () {
+    it('promise based way',  () => {
+        this.timeout(200)
+        return axios.get('https://reqres.in/api/users/2').then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in')
+        })
+    });
+
+    it('done based way',  function (done) {
+        // this.retries(5)
+
+        axios.get('https://reqres.in/api/users/2')
+            .then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+            done();
+        })
+            .catch(err => {
+                done(err);
+            })
+    });
+
+    it('async await based way',  async () => {
+        const res = await axios.get('https://reqres.in/api/users/2');
+        expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+    });
+})
+
+describe.skip('1 async test case', function () {
+    it('promise based way',  () => {
+        this.timeout(200)
+        return axios.get('https://reqres.in/api/users/2').then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in')
+        })
+    });
+})
+
+describe.only('2 async test case', function () {
+    it('promise based way',  () => {
+        this.timeout(200)
+        return axios.get('https://reqres.in/api/users/2').then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in')
+        })
+    });
+})
